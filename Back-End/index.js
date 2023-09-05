a4c14a8dcc9a1b8fc8635f5016a91a04d06e004a
Yaguarete4 <111437151+Yaguarete4@users.noreply.github.com>
2023-09-05 12:06:27 -0300

    Base de daton y login

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,24 +1,62 @@
 require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql2');
+const connection = mysql.createPool(process.env.DATABASE_URL).promise();
 const app = express();
 
 app.use(express.json());
 
-const users = [];
-
-app.get('/users', (req, res) => {
-    res.json(users);
+// Middleware personalizado para habilitar CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Cambia esto al dominio que deseas permitir
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE'); // Métodos HTTP permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeceras permitidas
+    next();
 });
 
-app.post('/users', (req, res) => {
-    const user = {name: req.body.name, password: req.body.password};
-    users.push(user);
-    res.status(201).send();
+//-----------------------------------------------------------------------------------------------------------
+
+app.get('/users', async (req, res) => {
+    const [row] =  await connection.query('SELECT * FROM Users');
+    res.json(row);
+});
+
+app.post('/users', async (req, res) => {
+    let success = false;
+    let message;
+
+    const [row] =  await connection.query(`SELECT password FROM Users WHERE email = ?`, [req.body.email]);
+
+    if(row.length != 0) {
+        if(req.body.password == row[0].password){
+            success = true;
+            message = "Inicio de sesion exitoso";
+        } else {
+            success = false;
+            message = "La constraseña es incorrecta";
+        }
+    }
+    else {
+        message = "El correo no existe";
+    }
+
+    // res.json({
+    //     success: success,
+    //     message: message,
+    //     user: {
+    //         email: req.body.email
+    //     }
+    // });
+
+    res.json(req.body);
 });
 
 app.listen(3000);
-
-const mysql = require('mysql2');
-const connection = mysql.createConnection(process.env.DATABASE_URL);
 console.log('Connected to PlanetScale!');
-connection.end();
+//connection.end();
+
+// const checkBannedChars = (string, array) => {
+//     for(let i = 0; i < array.length; i++){
+//         if (string.includes(array[i])) return true;
+//     }
+// }
